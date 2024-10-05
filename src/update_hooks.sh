3e3f613ea9009fd7434e021d6bc794d3c94b8b11
Amruth <106125855+amruth-sn@users.noreply.github.com>
2024-10-04 21:10:35 -0400

    Create update_hooks.sh

---
+++ src/update_hooks.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+HOOK_PATH="$HOME/.git-templates/hooks/prepare-commit-msg"
+
+REPOS=(
+    # Update with the local directory paths to all of your cloned repos
+    # You should only have to do this once.
+)
+
+git config --global init.templatedir '~/.git-templates'
+for repo in "${REPOS[@]}"; do
+    if [ -d "$repo/.git" ]; then
+        cp "$HOOK_PATH" "$repo/.git/hooks/prepare-commit-msg"
+        chmod +x "$repo/.git/hooks/prepare-commit-msg"
+        echo "Updated hook for $repo"
+
+    fi
+done
